e183d81a6af08db6f916b9e2ac4cc2ee93daaf27
Andy <musictopia2@hotmail.com>
2021-12-06 15:53:44 -0700

    Add project files.

---
+++ MySourceGenerator.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Text;
+namespace GenerateHelperLibraries;
+[Generator]
+public class MySourceGenerator : ISourceGenerator
+{
+    private void AppendToBuilder(ref StringBuilder builder, SyntaxTree tree)
+    {
+        string text = tree.ToString();
+        string fileName = tree.GetFileNameForCopy();
+        string spaces = "        ";
+        string content = text.GetCSharpString();
+        content = content.RemoveAttribute("IncludeCode");
+        content = content.RemoveAttribute("IgnoreCode");
+        builder.AppendLine($@"{spaces}text = @""{content}"";");
+        builder.AppendLine($@"{spaces}context.AddSource(""{fileName}"", text);");
+    }
+    public void Execute(GeneratorExecutionContext context)
+    {
+        Compilation compilation = context.Compilation;
+        bool includeglobal = compilation.DidIncludeCodeAtLeastOnce();
+        StringBuilder builder = new();
+        builder.AppendLine("global using SourceCodeHelpers.Utilities;");
+        builder.AppendLine("using Microsoft.CodeAnalysis;");
+        builder.AppendLine("namespace SourceCodeHelpers.Utilities;");
+        builder.AppendLine("public static class BuilderExtensions");
+        builder.AppendLine("{");
+        builder.AppendLine("    public static void BuildSourceCode(this GeneratorExecutionContext context)");
+        builder.AppendLine("    {");
+        builder.AppendLine("        string text;");
+        bool hadOne = false;
+        foreach (var item in compilation.SyntaxTrees)
+        {
+
+            if (item.ToString().Contains("namespace ") == false)
+            {
+                continue; //because there was none.
+            }
+            if (item.ToString().Contains("ISourceGenerator"))
+            {
+                continue; //you cannot generate source code for itself obviously
+            }
+            if (item.ToString().Contains("ISyntaxReceiver"))
+            {
+                continue; //you cannot generate source code for syntax receivers
+            }
+            bool includSingle = item.DidIncludeCode();
+            bool ignoreSingle = item.DidIgnoreCode();
+            if (ignoreSingle && includSingle)
+            {
+                string error = "Cannot include and ignore code at the same time";
+                //not reporting errors (has to test that next).
+                context.ReportError(error, "IgnoreIncludeConflict");
+                return;
+            }
+            if (item.DidIgnoreCode() && includeglobal == true)
+            {
+                string error = "Unable to generate source code because you ignored code even though you manually marked some to include";
+                context.ReportError(error, "IgnoreIncludeConflict");
+                return;
+            }
+            if (ignoreSingle)
+            {
+                continue; //because its being ignored.
+            }
+            if (includeglobal == true && includSingle == false)
+            {
+                continue;
+            }
+            hadOne = true;
+            AppendToBuilder(ref builder, item);
+        }
+        if (hadOne == false)
+        {
+            return;
+        }
+        builder.AppendLine("    }");
+        builder.AppendLine("}");
+        string results = builder.ToString();
+        context.AddSource("importlibrary.g", results);
+    }
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        
+    }
+}
